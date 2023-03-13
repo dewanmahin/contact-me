6e58e2276d4fbadefb4822d4d7843029ec2b56ee
DEWAN <dewanmahinchy@gmail.com>
2023-03-14 02:41:29 +0600

    send all info to local storage by containing all data in a object

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 // get input value using a function
-const getInputValue = id => {
-    return document.getElementById(id).value;
+const getInputValue = key => {
+    const value = document.getElementById(key).value;
+    const inputData = {key, value};
+    return inputData;
 }
 // Send info in local storage
 const sendInfo = (key) => {
-    console.log(key)
     const inputValue = getInputValue(key);
-    if(key == 'name'){
-        localStorage.setItem('name', inputValue);
-    }else if(key == 'email'){
-        localStorage.setItem('email', inputValue);
+    if(inputValue.key == 'name'){
+        localStorage.setItem('name', inputValue.value);
+    }else if(inputValue.key == 'email'){
+        localStorage.setItem('email', inputValue.value);
     }else{
-        localStorage.setItem('message', inputValue);
+        localStorage.setItem('message', inputValue.value);
     }
 }
 // delete info from local storage
@@ -21,4 +22,26 @@ const deleteInfo = (key) => {
 // clear all local storage info
 const restAllInfo = () => {
     localStorage.clear();
+}
+// get obj if available or return an empty obj
+const formDataObj = () => {
+    const formObj = localStorage.getItem('formData');
+    const formData = {};
+    if(formObj){
+        formData = JSON.parse(formObj);
+    }
+    return formData;
+}
+// send all info to local storage in a object
+const sendAllInfo = () => {
+    const formData = formDataObj(); // calling obj
+    const nameData = getInputValue('name');
+    const emailData = getInputValue('email');
+    const messageData = getInputValue('message');
+    const allInfo = [nameData, emailData, messageData];
+    allInfo.forEach(info => {
+        formData[info.key] = info.value;
+    });
+    const formDataStringified = JSON.stringify(formData);
+    localStorage.setItem('formData', formDataStringified);
 }
