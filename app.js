9ff33dc81f7c9c8dd2e4ef305d1b5a97f84684c0
DEWAN <dewanmahinchy@gmail.com>
2023-03-14 00:41:51 +0600

    send value on specific key

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const getInputValue = id => {
 }
 // Send info in local storage
 const sendInfo = (key) => {
+    console.log(key)
     const inputValue = getInputValue(key);
-
-    localStorage.setItem('name', inputValue);
+    if(key == 'name'){
+        localStorage.setItem('name', inputValue);
+    }else if(key == 'email'){
+        localStorage.setItem('email', inputValue);
+    }else{
+        localStorage.setItem('message', inputValue);
+    }
 }
